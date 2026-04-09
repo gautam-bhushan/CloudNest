201471d7f0d796fe765cc60e6aac7c21d4b8f1eb
Gautam Bhushan <148893478+gautam-bhushan@users.noreply.github.com>
2026-04-10 03:26:21 +0530

    Refactor server.js for improved structure

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fs = require("fs-extra");
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
+app.use(express.static(__dirname));
 
 fs.ensureDirSync("client_uploads");
 
@@ -72,5 +73,3 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-
-app.use(express.static(__dirname));
